a178264857bcc5a01251793a63f1f18c4e2ef974
olgagrts <olagricacueva@gmail.com>
2023-04-30 13:12:21 +0300

    add spring-security add jwt

---
+++ src/main/java/com/haha/app/controller/AuthenticationController.java
@@ -0,0 +1,36 @@
+package com.haha.app.controller;
+
+import com.haha.app.dto.AuthenticationRequest;
+import com.haha.app.dto.AuthenticationResponse;
+import com.haha.app.dto.RegisterRequest;
+import com.haha.app.service.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationService service;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody RegisterRequest request
+    ) {
+        return ResponseEntity.ok(service.register(request));
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody AuthenticationRequest request
+    ) {
+        return ResponseEntity.ok(service.authenticate(request));
+    }
+
+
+}
--- src/main/java/com/haha/app/controller/HahaAppController.java
+++ src/main/java/com/haha/app/controller/HahaAppController.java
@@ -3,19 +3,21 @@ package com.haha.app.controller;
 import com.haha.app.model.Setup;
 import com.haha.app.service.SetupService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("api/v1/setup")
 public class HahaAppController {
 
     @Autowired
     SetupService setupService;
 
     @GetMapping("/getAllSetups")
-    public List<Setup> getTestData() {
-        return setupService.getSetups();
+    public ResponseEntity<List<Setup>> getTestData() {
+        return ResponseEntity.ok(setupService.getSetups());
     }
 
 }
